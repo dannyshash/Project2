545c187f5f7773f58c28612955f1b13a7e3281c3
htl1991 <htl1991@users.noreply.github.com>
2019-01-27 09:35:13 -0500

    Initial Commit

---
+++ COMP5541Project1/src/ExpenseList.java
@@ -0,0 +1,34 @@
+/**
+ * Description: The ExpenseList is an ArrayList where Expense class will be loaded unto.
+ * Author: Tony Lac
+ * Created Date: 2019-01-25
+ */
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.io.*;
+
+public class ExpenseList {
+
+	private ArrayList<Purchase> purchaseList;
+	
+	public ExpenseList() {
+		purchaseList = new ArrayList<Purchase>();
+	}
+	
+	public void add(Purchase pl) {
+		purchaseList.add(pl);
+	}
+	
+	public void remove(int index) {
+		purchaseList.remove(index);
+	}
+	
+	public void markPaidUnpaid(int index) {
+		purchaseList.get(index).setStatus();
+	}
+
+	public ArrayList<Purchase> getList() {
+		return purchaseList;
+	}
+}
