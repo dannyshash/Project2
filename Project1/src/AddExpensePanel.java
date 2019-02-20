0da71de6000af87301069eebd13ea0341eb20330
htl1991 <45964798+htl1991@users.noreply.github.com>
2019-02-20 07:36:56 -0500

    Increment2 Prototype

--- Project1/src/AddExpensePanel.java
+++ Project1/src/AddExpensePanel.java
@@ -50,7 +50,7 @@ public class AddExpensePanel extends UserInterface {
 		
 		setTitle("Add Expense Panel");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 330, 526);
+		setBounds(100, 100, 360, 575);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -58,51 +58,51 @@ public class AddExpensePanel extends UserInterface {
 	
 		// Label Creation
 				JLabel lblNewLabel = new JLabel("Type");
-				lblNewLabel.setBounds(30, 73, 53, 23);
+				lblNewLabel.setBounds(30, 50, 53, 23);
 				contentPane.add(lblNewLabel);
 
 				JLabel lblDate = new JLabel("Date");
-				lblDate.setBounds(30, 150, 53, 23);
+				lblDate.setBounds(30, 180, 53, 23);
 				contentPane.add(lblDate);
 
 				JLabel lblName = new JLabel("Name");
-				lblName.setBounds(30, 185, 53, 23);
+				lblName.setBounds(30, 215, 53, 23);
 				contentPane.add(lblName);
 
 				JLabel lblAmount = new JLabel("Amount");
-				lblAmount.setBounds(30, 220, 53, 23);
+				lblAmount.setBounds(30, 250, 53, 23);
 				contentPane.add(lblAmount);
 
 				JLabel lblStatus = new JLabel("Status");
-				lblStatus.setBounds(30, 255, 53, 23);
+				lblStatus.setBounds(30, 285, 53, 23);
 				contentPane.add(lblStatus);
 
 				JLabel lblLocation = new JLabel("Location");
-				lblLocation.setBounds(30, 290, 53, 23);
+				lblLocation.setBounds(30, 320, 53, 23);
 				contentPane.add(lblLocation);
 
 				JLabel lblMethod = new JLabel("Method");
-				lblMethod.setBounds(30, 325, 53, 23);
+				lblMethod.setBounds(30, 355, 53, 23);
 				contentPane.add(lblMethod);
 
 				JLabel lblDueDate = new JLabel("Due Date");
-				lblDueDate.setBounds(30, 360, 53, 23);
+				lblDueDate.setBounds(30, 390, 53, 23);
 				contentPane.add(lblDueDate);
 
 				JLabel lblInterval = new JLabel("Interval");
-				lblInterval.setBounds(30, 395, 53, 23);
+				lblInterval.setBounds(30, 425, 53, 23);
 				contentPane.add(lblInterval);
 
 				// Type
 				JComboBox comboBox3 = new JComboBox(new String[] { "Purchase", "Bill" });
-				comboBox3.setBounds(93, 74, 200, 20);
+				comboBox3.setBounds(147, 51, 174, 20);
 				comboBox3.setSelectedIndex(0);
 				comboBox3.setMaximumRowCount(2);
 				contentPane.add(comboBox3);
 
 				// Date
 				JTextField textField_1 = new JTextField(dateFormat.format(new Date((2018 - 1900), (int)(Math.round(Math.random() * (12 - 1))), (int)(Math.round(Math.random() * (28 - 1))))));
-				textField_1.setBounds(90, 150, 200, 20);
+				textField_1.setBounds(147, 181, 174, 20);
 				textField_1.setColumns(10);
 				contentPane.add(textField_1);
 
@@ -110,19 +110,19 @@ public class AddExpensePanel extends UserInterface {
 				String[] randomName = { "Tea", "Coffee", "Sandwich", "Cookie", "Toast", "Chocolate",
 						"Candy"};
 				JTextField textField_2 = new JTextField(randomName[new Random().nextInt(randomName.length)]);
-				textField_2.setBounds(90, 185, 200, 20);
+				textField_2.setBounds(147, 216, 174, 20);
 				textField_2.setColumns(10);
 				contentPane.add(textField_2);
 
 				// Amount
 				JTextField textField_3 = new JTextField("" + (Math.round(Math.random() * 1000.0) / 100.0));
-				textField_3.setBounds(90, 220, 200, 20);
+				textField_3.setBounds(147, 251, 174, 20);
 				textField_3.setColumns(10);
 				contentPane.add(textField_3);
 
 				// Status
 				JComboBox comboBox2 = new JComboBox(new String[] { "Unpaid", "Paid" });
-				comboBox2.setBounds(90, 255, 200, 20);
+				comboBox2.setBounds(147, 285, 174, 20);
 				comboBox2.setSelectedIndex(1);
 				comboBox2.setMaximumRowCount(2);
 				contentPane.add(comboBox2);
@@ -130,20 +130,20 @@ public class AddExpensePanel extends UserInterface {
 				// Location
 				String[] randomLocation = { "Tim Hortons", "Starbucks", "Second Cup", "Van Houtte"};
 				JTextField textField_5 = new JTextField(randomLocation[new Random().nextInt(randomLocation.length)]);
-				textField_5.setBounds(90, 290, 200, 20);
+				textField_5.setBounds(147, 321, 174, 20);
 				textField_5.setColumns(10);
 				contentPane.add(textField_5);
 
 				// Method
 				JComboBox comboBox = new JComboBox(new String[] { "Cash", "Debit", "Credit Card" });
-				comboBox.setBounds(90, 325, 200, 20);
+				comboBox.setBounds(147, 356, 174, 20);
 				comboBox.setSelectedIndex((int)(Math.random()*3));
 				comboBox.setMaximumRowCount(3);
 				contentPane.add(comboBox);
 				
 				// Due Date
 				JTextField textField_7 = new JTextField(dateFormat.format(new Date()));
-				textField_7.setBounds(90, 360, 200, 20);
+				textField_7.setBounds(147, 391, 174, 20);
 				textField_7.setColumns(10);
 				contentPane.add(textField_7);
 				
@@ -151,14 +151,40 @@ public class AddExpensePanel extends UserInterface {
 				JComboBox comboBox4 = new JComboBox(new String[] { "Weekly", "Monthly", "Yearly" });
 				comboBox4.setSelectedIndex(1);
 				comboBox4.setMaximumRowCount(3);
-				comboBox4.setBounds(90, 395, 200, 20);
+				comboBox4.setBounds(147, 426, 174, 20);
 				contentPane.add(comboBox4);
+				
+				// Category
+				JLabel lblCategory = new JLabel("Category");
+				lblCategory.setBounds(30, 80, 53, 23);
+				contentPane.add(lblCategory);
+				
+				JComboBox comboBox5 = new JComboBox(new String[] { "Single", "Periodic", "Composite" });
+				comboBox5.setSelectedIndex(0);
+				comboBox5.setMaximumRowCount(3);
+				comboBox5.setBounds(147, 81, 174, 20);
+				contentPane.add(comboBox5);
+				
+				// Category Name
+				JLabel lblCategoryname = new JLabel("Category Name");
+				lblCategoryname.setBounds(30, 110, 107, 23);
+				contentPane.add(lblCategoryname);
+				
+				
+				JTextField textField_8 = new JTextField("");
+				textField_8.setBounds(147, 111, 174, 20);
+				textField_8.setColumns(10);
+				contentPane.add(textField_8);
+
 				
 				// Default Hide DueDate and Interval
 				textField_7.setVisible(false);
 				comboBox4.setVisible(false);
+				textField_8.setVisible(false);
 				lblDueDate.setVisible(false);
 				lblInterval.setVisible(false);
+				lblCategoryname.setVisible(false);
+				
 				
 				comboBox3.addActionListener(new ActionListener() {
 		            public void actionPerformed(ActionEvent event) {
@@ -175,19 +201,40 @@ public class AddExpensePanel extends UserInterface {
 							lblDueDate.setVisible(false);
 							lblInterval.setVisible(false);
 						}
-		            }
+		            	
+		
+	            }
+		            
+		            
+		         });
+				
+				
+				comboBox5.addActionListener(new ActionListener() {
+		            public void actionPerformed(ActionEvent event) {
+		            	
+		            	
+		             	if(comboBox5.getSelectedItem().toString() == "Composite") {
+		             		lblCategoryname.setVisible(true);
+		             		textField_8.setVisible(true);
+						}
+						else {
+							lblCategoryname.setVisible(false);
+							textField_8.setVisible(false);
+						}
+		            
+	            }
 		            
 		         });
 				
 				JButton btnAddExpense1 = new JButton("Add Expense");
-				btnAddExpense1.setBounds(182, 442, 108, 23);
+				btnAddExpense1.setBounds(213, 473, 108, 23);
 				contentPane.add(btnAddExpense1);
 				
 				txtChooseExpense = new JTextField();
 				txtChooseExpense.setBorder(null);
 				txtChooseExpense.setBackground(UIManager.getColor("Button.background"));
 				txtChooseExpense.setText("1. Choose Expense Type");
-				txtChooseExpense.setBounds(30, 42, 260, 20);
+				txtChooseExpense.setBounds(30, 20, 260, 20);
 				contentPane.add(txtChooseExpense);
 				txtChooseExpense.setColumns(10);
 				
@@ -196,9 +243,10 @@ public class AddExpensePanel extends UserInterface {
 				txtEnterExpense.setBackground(UIManager.getColor("Button.background"));
 				txtEnterExpense.setText("2. Enter Expense Details");
 				txtEnterExpense.setColumns(10);
-				txtEnterExpense.setBounds(30, 119, 260, 20);
+				txtEnterExpense.setBounds(30, 149, 260, 20);
 				contentPane.add(txtEnterExpense);
-
+				
+			
 				btnAddExpense1.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 
@@ -217,7 +265,7 @@ public class AddExpensePanel extends UserInterface {
 						Purchase purchaseAdd = new Purchase(comboBox3.getSelectedItem().toString(),
 								new Date((textField_1_year - 1900), textField_1_month - 1, textField_1_day),
 								textField_2.getText(), new Double(textField_3.getText()),
-								comboBox2.getSelectedItem().toString(), textField_5.getText(),
+								comboBox2.getSelectedItem().toString(), comboBox5.getSelectedItem().toString(), textField_8.getText(), textField_5.getText(),
 								comboBox.getSelectedItem().toString(),
 								new Date((textField_7_year - 1900), textField_7_month - 1, textField_7_day));
 						
@@ -225,7 +273,7 @@ public class AddExpensePanel extends UserInterface {
 						Bill billAdd = new Bill(comboBox3.getSelectedItem().toString(),
 								new Date((textField_1_year - 1900), textField_1_month - 1, textField_1_day),
 								textField_2.getText(), new Double(textField_3.getText()),
-								comboBox2.getSelectedItem().toString(), textField_5.getText(),
+								comboBox2.getSelectedItem().toString(), comboBox5.getSelectedItem().toString(), textField_8.getText(), textField_5.getText(),
 								comboBox.getSelectedItem().toString(),
 								new Date((textField_7_year - 1900), textField_7_month - 1, textField_7_day), comboBox4.getSelectedItem().toString());
 						
